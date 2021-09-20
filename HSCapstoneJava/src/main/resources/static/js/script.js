54e4474ef5124d40c1b73a92cfbc5e7dd8f9d105
Henry Vuong <khoa.vinh2000@gmail.com>
2021-09-20 15:49:18 -0400

    Revert "Test Commit"
    This reverts commit d0835c0c044ce14deb4c22ab882f325969ceecbc.

--- HSCapstoneJava/src/main/resources/static/js/script.js
+++ HSCapstoneJava/src/main/resources/static/js/script.js
@@ -1,8 +1,6 @@
 /**
  * 
  */
- var userInput = "";
- 
 function verifyDeleteOrder(x, y) {
     var x = confirm("Are you sure you would like to delete order #" + x + " for " + y);
 	if(x == true){
@@ -33,10 +31,6 @@ function verifyDeleteService(x){
 	}
 }
 
-function emailLowerCase(id) {
-  var x = document.getElementById(id);
-  x.value = x.value.toLowerCase();
-}
 
 
 function verifyDeleteWorker(x) {
@@ -48,34 +42,4 @@ function verifyDeleteWorker(x) {
 		return false;
 	}
 
-}
-
-function isNumberKey(evt) {
-    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
-    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
-        return false;
-    return true;
-}
-
-function onFocusOutFormat (id){
-	var formattedInput = "";
-	var input = document.getElementById(id);
-	var inputLength = document.getElementById(id).value.length;
-	console.log(inputLength);
-
-	if ( inputLength == 10 ) {
-
-		formattedInput = "(" + input.value.substr(0,3) + ") " + input.value.substr(3,3) + "-" + input.value.substr(6,4);
-		input.value = formattedInput;
-	}
-}
-function onFocusFormat (id){
-	var input = document.getElementById(id);
-	var unformatted = "";
-	if ( input.value.match("[(][0-9]{3}[)][ ][0-9]{3}-[0-9]{4}") ){
-
-		unformatted = input.value.substr(1,3) + input.value.substr(6,3) + input.value.substr(10,4);
-		input.value = unformatted;
-	}
-	
 }
